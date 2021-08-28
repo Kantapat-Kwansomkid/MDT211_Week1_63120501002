24df08c10978f263a23938cce0c9977b56e280be
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-08-28 19:10:19 +0700

    Add files via upload

---
+++ MDT112_Quiz1_63120501002.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MDT112_Quiz1_63120501002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+ 
+            int mul = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= 1; i++)
+            {
+                Console.WriteLine("  " + mul + " x " + i + " = " + mul * i);
+            }
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+
+            int mul = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("  " + mul + " x " + i + " = " + mul * i);
+            }
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+
+            int mul = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("  " + mul + " x " + i + " = " + mul * i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
